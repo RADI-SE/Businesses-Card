23abd272bd0a2bb445b11ebb766e631e4aeccdde
RADI-SE <radi1y@hotmail>
2023-10-08 01:29:44 +0300

    Coment removed

--- app/src/main/java/com/example/Assignment_01/MainActivity.kt
+++ app/src/main/java/com/example/Assignment_01/MainActivity.kt
@@ -32,7 +32,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Assignment_01Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
